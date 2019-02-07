36f2c762fc0cb3fbd280c2f0ddc3fda03153e77f
Matthew <Matthew@Matthews-MacBook-Pro.local>
2019-02-07 10:46:01 +0800

    Updates

---
+++ component---src-pages-404-js-f3e0ba6380c6f785bb1c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{148:function(t,e,n){"use strict";n.r(e);var i=n(0),a=n.n(i),s=n(158);n(159);e.default=function(){return a.a.createElement(s.d,{title:"404",subtitle:"NOT FOUND",details:"我們未能找到你需要的頁面"})}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f3e0ba6380c6f785bb1c.js.map
